5443f7b755e00b4f7bc78e234d97aac6f8787f75
Harsh Soni <algebra.soni@gmail.com>
2024-04-21 12:06:04 +0000

    feat: schedule fetching price of ethereum every 10 minutes

--- TASK_2/index.js
+++ TASK_2/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const transactionController = require('./api/etherscanAPI');
 const mongoose = require('mongoose');
+const cron = require('node-cron');
+const fetchPrice = require('./api/priceFetcherAPI');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,3 +16,6 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 // api endpoint to get transactions for input address
 app.get('/transactions/:address', transactionController.getTransactions);
 app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+// schedule fetching price of ethereum every 10 minutes
+cron.schedule('*/10 * * * *', fetchPrice);

969de04245d75270ba7909a9317ca4ce51395ac0
Harsh Soni <algebra.soni@gmail.com>
2024-04-21 11:17:47 +0000

    chore: copy codebase from TASK_1 to TASK_2

---
+++ TASK_2/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const transactionController = require('./api/etherscanAPI');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+// api endpoint to get transactions for input address
+app.get('/transactions/:address', transactionController.getTransactions);
+app.listen(port, () => console.log(`Server listening on port ${port}`));

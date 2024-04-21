dedcdecd0bbbbe2b617780844ca9716164dce342
Harsh Soni <algebra.soni@gmail.com>
2024-04-21 10:57:15 +0000

    feat: connect to mongodb

--- TASK_1/index.js
+++ TASK_1/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const transactionController = require('./api/etherscanAPI');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 // api endpoint to get transactions for input address
 app.get('/transactions/:address', transactionController.getTransactions);
